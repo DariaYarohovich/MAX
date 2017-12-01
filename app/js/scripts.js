9e30986ba0fc883b9609d4b87a6f6fb4142f503c
mozolevsky <alf.rulit@gmail.com>
2017-12-02 01:31:26 +0300

    Made submenu collapse in sideBar

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,1 +1,22 @@
 "use strict";
+
+// sideBar submenus
++(() => {
+    const parentMenuItems = document.querySelectorAll('.navigation__item_parent');
+
+    for (let i = 0; i < parentMenuItems.length; i++) {
+        parentMenuItems[i].addEventListener("click", () => {
+
+            let parentMenuItem = parentMenuItems[i];
+            parentMenuItem.classList.toggle("navigation__item_active-parent");
+
+            let subMenu = parentMenuItem.querySelector(".navigation__list_second");
+            if (subMenu.style.maxHeight) {
+                subMenu.style.maxHeight = null;
+            } else {
+                subMenu.style.maxHeight = subMenu.scrollHeight + "px";
+            }
+        });
+    }
+})();
+
