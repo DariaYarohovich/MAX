d14b7bd6672adfb9a739bfed7ab1d6a30a92f757
mozolevsky <alf.rulit@gmail.com>
2017-12-02 04:47:51 +0300

    Video custom controls, collapsing blocs, style adjustments

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -5,9 +5,9 @@
     const parentMenuItems = document.querySelectorAll('.navigation__item_parent');
 
     for (let i = 0; i < parentMenuItems.length; i++) {
-        parentMenuItems[i].addEventListener("click", () => {
 
-            let parentMenuItem = parentMenuItems[i];
+        let parentMenuItem = parentMenuItems[i];
+        parentMenuItem.addEventListener("click", () => {
             parentMenuItem.classList.toggle("navigation__item_active-parent");
 
             let subMenu = parentMenuItem.querySelector(".navigation__list_second");
@@ -20,6 +20,39 @@
     }
 })();
 
+// videos 
++(() => {
+    const videoBlocksCaps = document.querySelectorAll(".video-block__cap");
+
+    for (let i = 0; i < videoBlocksCaps.length; i++) {
+        let videoBlockCap = videoBlocksCaps[i];
+
+        videoBlockCap.addEventListener("click", () => {
+            videoBlockCap.style = "display: none";
+
+            const video = videoBlockCap.previousElementSibling;
+            video.setAttribute("controls", "");
+            video.play();
+            video.volume = 0.5;
+        });
+    }
+})();
+
+// collapse block
++(() => {
+    const collapseBtn = document.querySelector(".collapse-btn");
+    const collapseArea = document.querySelector(".collapse-area");
+
+    collapseBtn.addEventListener("click", () => {
+        if (collapseArea.style.maxHeight) {
+            collapseArea.style.maxHeight = null;
+        } else {
+            collapseArea.style.maxHeight = collapseArea.scrollHeight + "px";
+        }
+    });
+})();
+
 $( function() {
     $( "#video-tabs" ).tabs();
 } );
+
